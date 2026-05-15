7838fb9a0634764f5ef412797fe8d556ca1fb67c
mahesa <mahesaagsa101@gmail.com>
2026-05-15 21:18:55 +0700

    web server in node js comment

--- 9_NodeJs/web.js
+++ 9_NodeJs/web.js
@@ -1,23 +1,33 @@
-const http = require("http");
-const fs = require("fs");
+const http = require("http");//mengambil fungsi http atau berhubungan dengan server memalui core module
+const fs = require("fs");//mengmabil fungsi mengelolah file system di core module
 const port = 3333;
+//bertujuan untuk membuat template agar lebih mudah dalam membuat satu page web
 const renderHTML = (path, res) => {
   fs.readFile(path, (error, data) => {
+    //pertama di baca dulu file yang di isi
+    //setelah itu meliha kondisi jika error maka ini aktif
     if (error) {
       res.writeHead(404);
       res.write("Error: file no found");
+      //jika tidak ini aktif
     } else {
+        //cara agar web dapat menampil kan data yang diberikan
       res.write(data);
     }
+    //memberi tau server atau web bahwa semua nya sudah selesia atau siap
     res.end();
   });
 };
-
+//membuat server local
 http
+//dimana memiliki dua parameter yaitu req apa yang user atau server terima
+//dan res apa yang server berikan
   .createServer((req, res) => {
+    //memberi tau status serta jenis data
     res.writeHead(200, {
       "content-type": "text/html",
     });
+    //menerima nilai yang di berikan user
     const url = req.url;
 
     //cara switch
@@ -42,6 +52,7 @@ http
     //   renderHTML("./index.html", res);
     // }
   })
+  //untu menjalankan servernnya
   .listen(port, () => {
     console.log(`server is listening on port ${port}`);
   });
